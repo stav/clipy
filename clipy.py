5b2cd7a26495848ad782aa6561f86c825588e37a
Steven Almeroth <sroth77@gmail.com>
2014-07-21 14:33:37 -0500

    add stream selection

--- clipy.py
+++ clipy.py
@@ -10,20 +10,21 @@ TITLE = '.:. Clipy .:.'
 
 class Window(object):
     """Window absraction with border"""
+    stream = None
+    video = None
 
     def __init__(self, stdscr, lines, cols, y, x):
         self.stdscr = stdscr
-        self.stream = None
         self.box = curses.newwin(lines, cols, y, x)
         Y, X = self.box.getmaxyx()
         self.win = self.box.subwin(Y-2, X-4, 2, 2)
 
         self.box.box()
-        # self.coord(Y-4, X-5)
+        # self._coord(Y-4, X-5)
         # for i in range(Y-2):
-        #     self.coord(i, i)
+        #     self._coord(i, i)
 
-    def coord(self, y, x):
+    def _coord(self, y, x):
         self.win.addstr(y, x, '+ {} {}'.format(y, x))
 
     def freshen(self):
@@ -65,8 +66,40 @@ def inquire(window):
         window.printstr(e, error=True)
 
 
+def select(window):
+    if window.video:
+        window.printstr('Streams:')
+        for i, stream in enumerate(window.video.allstreams):
+            window.printstr('{}: {} {} {} {}'.format(i, stream.mediatype, stream.quality, stream.extension, stream.notes))
+        window.printstr('Press one of the numbers above to download (0-{})'.format(i))
+
+    else:
+        window.printstr('No video to select streams, Inquire first', error=True)
+
+
+def choice(window, index):
+    window.printstr('Choice')
+    download_dir = os.path.expanduser('~')
+    stream = window.video.allstreams[index]
+    window.stream = stream
+    try:
+        path = '%s.%s' % (os.path.join(download_dir, stream.title), stream.extension)
+        window.printstr('Downloading {} to {}'.format(stream, download_dir))
+        f = stream.download(filepath=path, quiet=True, callback=window.progress)
+
+    except (OSError, ValueError, FileNotFoundError) as e:
+        window.printstr(e, error=True)
+
+    else:
+        if f:
+            window.printstr('Downloaded: "{}"'.format(f))
+
+
 def download(window):
     download_dir = os.path.expanduser('~')
+    if window.video is None:
+        window.printstr('No video to download, Inquire first', error=True)
+        return
     try:
         best = window.video.getbest(preftype="mp4")
         window.stream = best
@@ -94,8 +127,10 @@ def cancel(window):
 def loop(stdscr, console):
     KEYS_QUIT = (ord('q'), ord('Q'), 27)  # 27 is escape
     KEYS_INQUIRE = (ord('i'), ord('I'))
+    KEYS_SELECT = (ord('s'), ord('S'))
     KEYS_DOWNLOAD = (ord('d'), ord('D'))
     KEYS_CANCEL = (ord('c'), ord('C'))
+    KEYS_NUMERIC = range(49, 58)
 
     while True:
         c = console.getch()
@@ -106,16 +141,25 @@ def loop(stdscr, console):
         if c in KEYS_INQUIRE:
             inquire(console)
 
+        if c in KEYS_SELECT:
+            select(console)
+
         if c in KEYS_DOWNLOAD:
             t = threading.Thread(target=download, args=(console,))
             t.daemon = True
             t.start()
 
+        if c in KEYS_NUMERIC:
+            choice(console, c-48)
+
         if c in KEYS_CANCEL:
             cancel(console)
 
+        # Debug
+        # console.printstr(str(c))
         # console.printstr(threading.active_count())
 
+        # Refresh screen
         stdscr.noutrefresh()
         console.freshen()
         curses.doupdate()
@@ -125,23 +169,27 @@ def init(stdscr):
     if curses.has_colors():
         curses.start_color()
 
-    # menu_options = 'Press "I": inquire, "S": select, "D": download, "C": cancel, "Q": quit'
-    menu_options = 'Press "I": inquire, "D": download, "C": cancel, "Q": quit'
-
+    # Setup
+    menu_options = 'Press "I": inquire, "S": select, "D": download, "C": cancel, "Q": quit'
     # curses.curs_set(False)
     curses.init_pair(1, curses.COLOR_RED  , curses.COLOR_BLACK)
 
+    # Title bar at top
     stdscr.addstr(TITLE, curses.A_REVERSE)
     stdscr.chgat(-1, curses.A_REVERSE)
-    stdscr.addstr(curses.LINES-1, 0, menu_options)
-    stdscr.addstr(curses.LINES-1, curses.COLS-15, str(curses.getsyx()))
 
+    # Menu options at bottom
+    stdscr.addstr(curses.LINES-1, 0, menu_options)
+
+    # Create container class
     console = Window(stdscr, curses.LINES-2, curses.COLS, 1, 0)
 
+    # Refresh screen
     stdscr.noutrefresh()
     console.freshen()
     curses.doupdate()
 
+    # Enter event loop
     loop(stdscr, console)
 
 
