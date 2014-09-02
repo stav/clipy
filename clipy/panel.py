ce6f566b3cc6341ab109f3e1cc34ca34e13be65d
Steven Almeroth <sroth77@gmail.com>
2014-09-01 21:00:30 -0500

    break up ui to window & panel

---
+++ clipy/panel.py
@@ -0,0 +1,277 @@
+"""
+Clipy YouTube video downloader user interface: Panel
+"""
+import os
+import re
+import curses
+import asyncio
+import threading
+import subprocess
+import collections
+
+import pyperclip
+
+import clipy.video
+import clipy.request
+import clipy.youtube
+
+
+class File(object):
+    """file encapsulation"""
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Thread(object):
+    """thread encapsulation"""
+    def __init__(self, thread):
+        self.thread = thread
+        self.name = thread.name
+
+    def __str__(self):
+        return str('{}: ident {}, {}'.format(
+            self.name,
+            self.thread.ident,
+            'Alive' if self.thread.is_alive() else 'Dead',
+            'Daemon' if self.thread.daemon else '',
+        ))
+
+
+class Panel(object):
+    """docstring for Panel"""
+    testing = False
+    target_dir = ''
+    input_mode = False
+    input_text = ''
+
+    def __init__(self, loop, stdscr, detail, cache, console):
+        self.loop = loop
+        self.stdscr = stdscr
+        self.detail = detail
+        self.cache = cache
+        self.console = console
+        # Should be in respective inits
+        detail.panel = cache.panel = console.panel = self
+        cache.win.scrollok(False)
+        cache.reset()
+
+    def reset(self):
+        self.detail.reset()
+        self.cache.reset()
+        self.console.display()
+
+    def display(self):
+        self.detail.display()
+        self.cache.display()
+        self.console.freshen()
+        self.update()
+
+    def update(self):
+        if not self.testing:
+            curses.doupdate()
+
+    def load_cache(self):
+        cprint = self.console.printstr
+
+        if os.path.exists('clipy.lookups'):
+            self.cache.load_lookups()
+            cprint('Cache: lookups loaded')
+        else:
+            cprint('Cache: no lookups found, not loaded')
+
+        if os.path.exists('clipy.downloads'):
+            self.cache.load_downloads()
+            cprint('Cache: downloads loaded')
+        else:
+            cprint('Cache: no downloads found, not loaded')
+
+    def save_cache(self):
+        with open('clipy.lookups', 'w') as f:
+            for key in self.cache.lookups:
+                cache = self.cache.lookups[key]
+                f.write('{} {}\n'.format(cache.videoid, cache))
+
+        with open('clipy.downloads', 'w') as f:
+            for key in self.cache.downloads:
+                cache = self.cache.downloads[key]
+                f.write('{} {}\n'.format(cache.stream.url, cache))
+
+            self.console.printstr('Cache: saved')
+
+    @asyncio.coroutine
+    def clipboard(self):
+        cprint = self.console.printstr
+        try:
+            contents = pyperclip.paste()
+        except:
+            contents = None
+            cprint('Cannot seem to read the clipboad, try (I) input')
+        if contents:
+            contents = str(contents).strip()
+            cprint('Checking clipboard: {}'.format(contents))
+            yield from self.inquire(contents)
+        else:
+            cprint('Found nothing in clipboard')
+
+    @asyncio.coroutine
+    def inquire(self, resource):
+        cprint = self.console.printstr
+
+        if 'youtube.com/results?search' in resource:
+            yield from self.cache.load_search(resource)
+            return
+
+        cprint('Inquiring: {}'.format(resource))
+        try:
+            video = yield from clipy.youtube.get_video(
+                resource, target=self.target_dir)
+
+        except ValueError as ex:
+            cprint('Error: {}'.format(ex), error=True)
+
+        except ConnectionError as ex:
+            cprint('Error: {}'.format(ex), error=True)
+
+        else:
+            if video:
+                self.detail.video = video
+                self.cache.lookups[video.videoid] = video
+                self.cache.streams.index = None
+                self.display()
+
+    def wait_for_input(self):
+        # self.cache.win.nodelay(False)
+        return self.cache.win.getch()
+
+    def view(self, key):
+        # Load cache from disk
+        if key == ord('L'):
+            self.load_cache()
+
+        # Save cache to disk
+        elif key == ord('C'):
+            self.save_cache()
+
+        # Change cache view
+        elif key == curses.KEY_LEFT:
+            if self.cache.index > 0:
+                self.cache.index -= 1
+
+        # Change cache view
+        elif key == curses.KEY_RIGHT:
+            if self.cache.index < len(self.cache.caches) - 1:
+                self.cache.index += 1
+
+        # Intra-cache navigation
+        else:
+            v_index = self.cache.videos.index
+
+            # Move up the list
+            if key == curses.KEY_UP:
+                if v_index is None:
+                    self.cache.videos.index = 0
+                elif v_index > 0:
+                    self.cache.videos.index -= 1
+
+            # Move down the list
+            elif key == curses.KEY_DOWN:
+                if v_index is None:
+                    self.cache.videos.index = 0
+                elif v_index < len(self.cache.videos) - 1:
+                    self.cache.videos.index += 1
+
+    @asyncio.coroutine
+    def action(self):
+        """ The Enter key was pressed """
+        cprint = self.console.printstr
+
+        # If we are in input mode then just inquire
+        if self.input_mode:
+            self.input_mode = False
+            yield from self.inquire(self.input_text)
+            return
+
+        def play():
+            cprint('Playing {}'.format(path))
+            try:
+                subprocess.call(
+                    ['mplayer', "{}".format(path)],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL)
+            except AttributeError:
+                cprint("Can't play, Python2?")
+
+        v_index = self.cache.videos.index
+
+        if v_index is not None:
+            # Validate selected index
+            if v_index >= 0 and v_index < len(self.cache.videos):
+
+                # Search / Lookups action
+                if self.cache.videos is self.cache.searches or \
+                   self.cache.videos is self.cache.lookups:
+                    yield from self.inquire(list(self.cache.videos)[v_index])
+
+                # Streams action
+                elif self.cache.videos is self.cache.streams:
+                    yield from self.download(self.detail.video, v_index)
+
+                # Downloads action
+                elif self.cache.videos is self.cache.downloads \
+                  or self.cache.videos is self.cache.files:
+                    key = list(self.cache.videos)[v_index]
+                    path = self.cache.videos[key].path
+                    if os.path.exists(path):
+                        self.loop.run_in_executor(None, play)
+                    else:
+                        cprint('File no longer exists {}'.format(path),
+                               error=True)
+
+                # Actives action
+                elif self.cache.videos is self.cache.actives:
+                    try:
+                        key = list(self.cache.actives)[v_index]
+                        cprint('Cancelling download: {}'.format(
+                            self.cache.actives[key]))
+                        del self.cache.actives[key]
+                    except IndexError:
+                        cprint('Nothing to cancel')
+
+    @asyncio.coroutine
+    def download(self, video, index=None):
+        cprint = self.console.printstr
+
+        if video is None:
+            cprint('No video to download, Inquire first', error=True)
+            return
+
+        stream = video.stream = video.streams[index or 0]
+
+        if stream.url in self.cache.actives:
+            cprint('Stream already downloading: {}'.format(stream))
+            return
+
+        cprint('Downloading {}'.format(stream.path))
+
+        self.cache.actives[stream.url] = stream
+
+        _success, _length = yield from clipy.request.governed_download(
+            stream, self.cache.actives)
+
+        # and here we start our inline that would "normally" be in a callback
+
+        # Add to downloaded list
+        if _success:
+            self.cache.downloads[stream.url] = stream
+
+        # Remove from actives list if not already cencelled
+        if stream.url in self.cache.actives:
+            del self.cache.actives[stream.url]
+
+        # Update screen to show the active download is no longer active
+        self.cache.display()
+        cprint('Perhaps {} bytes were saved to {}'.format(_length, stream.path))
---
+++ clipy/window.py
@@ -0,0 +1,213 @@
+"""
+Clipy YouTube video downloader user interface: Window
+"""
+import os
+import re
+import curses
+import asyncio
+import threading
+import subprocess
+import collections
+
+import pyperclip
+
+import clipy.video
+import clipy.request
+import clipy.youtube
+
+
+class Window(object):
+    """
+    Window absraction with border
+    """
+    win = box = None
+    panel = None
+
+    def __init__(self, lines, cols, y, x):
+        # Border
+        self.box = curses.newwin(lines, cols, y, x)
+        Y, X = self.box.getmaxyx()
+        self.box.box()
+
+        # Inner window
+        Y, X = self.box.getmaxyx()
+        self.win = self.box.subwin(Y-2, X-3, y+1, x+2)
+        self.win.scrollok(True)
+        self.win.keypad(True)
+
+        # Debug
+        # self._coord(Y-4, X-5)
+        # for i in range(Y-2):
+        #     self._coord(i, i)
+
+    def _coord(self, y, x):
+        self.win.addstr(y, x, '+ {} {}'.format(y, x))
+
+    def display(self):
+        self.win.erase()
+        self.box.erase()
+        self.box.box()
+
+    def freshen(self):
+        self.box.noutrefresh()
+        self.win.noutrefresh()
+
+    def printstr(self, text='', success=False, error=False, wow=False):
+        string = '\n{}'.format(text)
+        if error:
+            self.win.addstr(string, curses.A_BOLD | curses.color_pair(1))
+        elif success:
+            self.win.addstr(string, curses.color_pair(2))
+        elif wow:
+            self.win.addstr(string, curses.A_STANDOUT)
+        else:
+            self.win.addstr(string)
+        # Display immediately
+        self.freshen()
+        self.panel.update()
+
+
+class DetailWindow(Window):
+    """
+    Window with video info
+    """
+    video = None
+
+    def reset(self):
+        self.video = None
+
+    def display(self):
+        super(DetailWindow, self).display()
+
+        if self.panel.input_mode:
+            self.printstr('Input your YouTube search string, then press Enter')
+            self.printstr('> {}'.format(self.panel.input_text))
+
+        elif self.video:
+            self.printstr(self.video.detail)
+            self.panel.cache.streams.clear()
+            for stream in self.video.streams:
+                self.panel.cache.streams[stream.url] = stream
+
+        self.freshen()
+
+
+class ListWindow(Window):
+    """
+    Window with cache storage and list capabilities
+    """
+    class CacheList(collections.OrderedDict):
+        """An ordered dictionary of videos"""
+        def __init__(self, name, title=''):
+            super(self.__class__, self).__init__()
+            self.index = None
+            self.name = name
+            self.title = title
+
+        def __str__(self):
+            return '{}: {} {}'.format(self.name, len(self), self.title)
+
+    index = 0
+    caches = ()
+    searches = lookups = streams = downloads = files = threads = actives = None
+    videos = None   # mis-named as videos, s/b cache or something
+
+    def reset(self):
+        (
+            self.searches,
+            self.lookups,
+            self.streams,
+            self.downloads,
+            self.files,
+            self.threads,
+            self.actives,
+
+        ) = self.caches = (
+
+            self.CacheList('Search'),
+            self.CacheList('Inquiries'),
+            self.CacheList('Streams'),
+            self.CacheList('Downloaded'),
+            self.CacheList('Files', self.panel.target_dir),
+            self.CacheList('Threads'),
+            self.actives or self.CacheList('Active'),
+        )
+        self.index = 0
+        self.videos = self.caches[self.index]
+
+    def display(self):
+        super(ListWindow, self).display()
+
+        # Setup our videos pointer
+        self.videos = self.caches[self.index]
+
+        # Files: manually build the files list here real-time
+        if self.videos is self.files:
+            self.files.clear()
+            for filename in sorted(os.listdir(self.panel.target_dir)):
+                path = os.path.join(self.panel.target_dir, filename)
+                if os.path.isfile(path) and not filename.startswith('.'):
+                    self.files[path] = clipy.panel.File(filename, path)
+
+        # Threads: manually build the threads list here real-time
+        if self.videos is self.threads:
+            self.threads.clear()
+            for thread in threading.enumerate():
+                self.threads[thread.name] = clipy.panel.Thread(thread)
+
+        # Header: Display list of caches
+        for i, cache in enumerate(self.caches):
+            attr = curses.A_STANDOUT if cache.name == self.videos.name else 0
+            self.win.addstr(' {} '.format(cache.name.upper()), attr)
+
+        # Rows: Display selected cache detail
+        self.win.addstr(2, 2, str(self.videos))
+        for i, key in enumerate(self.videos):
+            video = self.videos[key]
+            attr = curses.A_STANDOUT if i == self.videos.index else 0
+            try:
+                self.win.addstr(3+i, 0, str(video), attr)
+            except Exception:  # Python 3 -> except:
+                break
+
+        self.freshen()
+
+    @asyncio.coroutine
+    def load_search(self, url):
+        self.panel.console.printstr('Searching: {}'.format(url))
+        html = yield from clipy.request.get_text(url)
+        videoids = re.findall('data-context-item-id="([^"]+)"', html)
+        self.panel.console.printstr('Video Ids: {}'.format(videoids))
+
+        if videoids:
+            self.searches.clear()
+            for videoid in videoids:
+                if videoid != '__video_id__':
+                    video = yield from clipy.youtube.get_video(
+                        videoid, target=self.panel.target_dir)
+                    if video:
+                        self.searches[videoid] = video
+                        self.display()
+                        self.panel.update()
+
+    def load_lookups(self):
+        """ Load file from disk into cache """
+        with open('clipy.lookups', 'r') as f:
+            for line in f.readlines():
+                key, duration, title = line.split(maxsplit=2)
+
+                video = clipy.video.VideoDetail()
+                video.videoid = key
+                video.duration = duration
+                video.title = title.strip()
+
+                self.lookups[key] = video
+
+    def load_downloads(self):
+        """ Load file from disk into cache """
+        with open('clipy.downloads', 'r') as f:
+            for line in f.readlines():
+                stream = clipy.video.VideoDetail()
+                stream.url, stream.path = line.split(maxsplit=1)
+                stream.path = stream.path.strip()
+                self.downloads[stream.url] = stream
