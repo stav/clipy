cf351c2c530239ebeffcf2facb83e0a7a7c3339a
Steven Almeroth <sroth77@gmail.com>
2014-09-03 21:04:06 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='clipy',
-    version='0.9.4',
+    version='0.9.6',
     license='BSD',
     url='https://github.com/stav/clipy',
     download_url='https://github.com/stav/clipy/archive/master.zip',
