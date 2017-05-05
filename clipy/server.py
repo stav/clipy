e6309aa20741a0cf235967386aadc1c4bbd551fd
Steven Almeroth <sroth77@gmail.com>
2017-05-04 18:27:24 -0700

    normalize server code

--- clipy/server.py
+++ clipy/server.py
@@ -18,15 +18,58 @@ import jinja2
 
 import clipy.routes
 
-
-async def run_loop(app):
-    while app['server']['running']:
-        await asyncio.sleep(2)
+loop = asyncio.get_event_loop()
+loop.set_debug(True)
 
 
 class PollFilter(logging.Filter):
     def filter(self, record):
         return not record.getMessage().startswith('poll')
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+asyncio_logger = logging.getLogger('asyncio')
+asyncio_logger.setLevel(logging.DEBUG)
+asyncio_logger.addFilter(PollFilter())
+
+clipy_logger = logging.getLogger('clipy')
+clipy_logger.setLevel(logging.DEBUG)
+clipy_logger.addFilter(PollFilter())
+
+
+def init(app):
+    app['server'] = dict()
+    app['actives'] = dict()
+    clipy.routes.setup_routes(app)
+    aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('clipy', 'templates'))
+    app.router.add_static('/static/', path='static', name='static')
+    app.on_startup.append(on_startup)
+    app.on_cleanup.append(on_cleanup)
+    app.on_shutdown.append(on_shutdown)
+
+
+async def on_startup(app):
+    clipy_logger.info(f'startup: {app}')
+    sockets = app['server']['sockets']
+    clipy_logger.info(f'startup: {sockets}')  # [0].getsockname()
+    app['server']['running'] = True
+    clipy_logger.info('serving on http://{}:{}/'.format(*sockets[0].getsockname()))
+
+
+async def on_shutdown(app):
+    clipy_logger.info(f'shutdown: {app}')
+
+
+async def on_cleanup(app):
+    clipy_logger.info(f'cleanup: {app}')
+    app['actives'].clear()
+    app['server'].clear()
+
+
+async def run_loop(app):
+    while app['server']['running']:
+        await asyncio.sleep(2)
 
 
 def main():
@@ -35,46 +78,24 @@ def main():
     #     config = yaml.load(stream)
     #     logging.config.dictConfig(config['logging'])
 
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger('asyncio')
-    logger.setLevel(logging.DEBUG)
-    logger.addFilter(PollFilter())
-
-    async def on_startup(app):
-        logging.debug('@@@ startup')
-
-    async def on_cleanup(app):
-        logging.debug('@@@ cleanup')
-
-    async def on_shutdown(app):
-        logging.debug('@@@ shutdown')
-
-    loop = asyncio.get_event_loop()
-    loop.set_debug(True)
-
     app = aiohttp.web.Application(
+        debug=True,
         # logger=logger,
         # loop=loop,  # deprecated http://aiohttp.readthedocs.io/en/stable/web_reference.html#aiohttp.web.Application
     )
-    app['actives'] = dict()
-    app['server'] = dict(running=True)
-    clipy.routes.setup_routes(app)
-    aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('clipy', 'templates'))
-    app.router.add_static('/static/', path='static', name='static')
-    app.on_startup.append(on_startup)
-    app.on_cleanup.append(on_cleanup)
-    app.on_shutdown.append(on_shutdown)
     # aiohttp.web.run_app(app, host='127.0.0.1', port=7070)
-
-    handler = app.make_handler()
+    init(app)
+    handler = app.make_handler(loop=loop)
     f = loop.create_server(handler, '127.0.0.1', 7070)
     srv = loop.run_until_complete(f)
-    logging.info('serving on http://{}:{}/'.format(*srv.sockets[0].getsockname()))
+    app['server']['sockets'] = srv.sockets
     try:
-        # loop.run_forever()
+        loop.run_until_complete(app.startup())
         loop.run_until_complete(run_loop(app))
     except Exception as e:
         logging.error(e)
+    except KeyboardInterrupt:
+        pass
     finally:
         srv.close()
         loop.run_until_complete(srv.wait_closed())
